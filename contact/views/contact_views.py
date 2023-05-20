fe6da2c54481e5fc508789d9cf81623053a7e0e2
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-05-20 09:33:18 -0300

    style: added header, added site title

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -10,6 +10,8 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
+
     }
 
     return render(
@@ -23,10 +25,11 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
             Contact.objects, pk=contact_id, show=True)
 
-    
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - ' 
 
     context = {
         'contact': single_contact,
+        'site_title': contact_name,
     }
 
     return render(
