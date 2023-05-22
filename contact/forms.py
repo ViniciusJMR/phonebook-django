a5b488a847fac4072e5525edf50b82f1b9c10244
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-05-22 16:16:22 -0300

    feat: added simple validation on forms

--- contact/forms.py
+++ contact/forms.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django import forms
 from django.core.exceptions import ValidationError
 from . import models
@@ -25,6 +26,33 @@ class ContactForm(forms.ModelForm):
     #         'placeholder': 'Escreva Aqui',
     #     })
 
+    def clean(self): 
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid',
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Não insira ABC neste campo',
+                    code='invalid',
+                )
+            )
+        return first_name 
+
 
     class Meta:
         model = models.Contact 
@@ -38,4 +66,5 @@ class ContactForm(forms.ModelForm):
         #         }
         #     )
         # }
+        
     
