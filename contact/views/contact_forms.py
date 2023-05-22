780a21038ab88cdaaedba6f28fd6db7e10cffdb2
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-05-22 14:38:33 -0300

    feat: added dynamic forms in create contact

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from django import forms
+
+from contact.models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name','last_name','phone',)
+
+appname = 'create'
 
 def create(request):
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST)
+    else:
+        contact_form = ContactForm()
 
     context = {
-        
+        'form': contact_form
     }
 
     return render(
